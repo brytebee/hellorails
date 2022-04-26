a2b5980a9249d528bb46d2273feafd73e2ae8eb6
brytebee <brytebee@gmail.com>
2022-04-26 01:34:45 +0100

    :rotating_light: Fix linters
    Fixed linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/hello'
-  get '/greetings' to: 'custom_pages#hello'
+  get '/greetings', to: 'custom_pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
